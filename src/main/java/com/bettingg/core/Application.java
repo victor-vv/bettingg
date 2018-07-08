cd33b0b5fb0bfe2525090b4449b11bf407d3c814
victor-vv <vertiev.v@gmail.com>
2018-07-08 22:27:04 +0300

    minor fix

--- src/main/java/com/bettingg/core/Application.java
+++ src/main/java/com/bettingg/core/Application.java
@@ -3,29 +3,15 @@ package com.bettingg.core;
 import com.bettingg.core.dao.TeamDao;
 import com.bettingg.core.domain.Team;
 import com.bettingg.core.service.TeamService;
-import com.bettingg.core.util.HibernateFactory;
-import org.hibernate.Session;
 
 public class Application {
 
     public static void main(String[] args) {
         System.out.println("Hibernate tutorial");
 
-        Session session = HibernateFactory.getSessionFactory().openSession();
-
-        session.beginTransaction();
-
         TeamService teamService = new TeamService(new TeamDao());
-//        Team team = new Team();
-//        team.setName("AMKAR");
-//        team.setCity("PERM");
-//        session.save(team);
-//        session.getTransaction().commit();
         for (Team team : teamService.getCurrentTeams()) {
             System.out.println(team.getName());
         }
-        session.close();
-
-
     }
 }
--- src/main/java/com/bettingg/core/dao/TeamDao.java
+++ src/main/java/com/bettingg/core/dao/TeamDao.java
@@ -2,16 +2,31 @@ package com.bettingg.core.dao;
 
 import com.bettingg.core.domain.Team;
 import com.bettingg.core.util.HibernateFactory;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
+@Repository
 public class TeamDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
 
     public Team findById(int id) {
         return HibernateFactory.getSessionFactory().openSession().get(Team.class, id);
     }
 
     public List<Team> getCurrentTeams() {
-        return HibernateFactory.getSessionFactory().openSession().createQuery("From Team").list();
+        Session session = HibernateFactory.getSessionFactory().openSession();
+        Criteria criteria = session.createCriteria(Team.class);
+        return criteria.list();
     }
 }
